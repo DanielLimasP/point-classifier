839d92abb09fa2d1516ddd25991c731bd5709737
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-06-26 18:08:48 -0600

    Finished the NN point classifier

--- point-classifier.py
+++ point-classifier.py
@@ -90,6 +90,82 @@ class PointClassifier:
         plt.ylim([-1, 1])
         plt.show()
 
+    def train_nn(self):
+        h = 100
+        W = 0.01 * np.random.randn(D,h)
+        b = np.zeros((1,h))
+        W2 = 0.01 * np.random.randn(h,K)
+        b2 = np.zeros((1,K))
+        
+        # Hyperparameters again
+        learning_rate = 1e-0
+        reg = 1e-3
+
+        # Gradient descent loop
+        num_examples = X.shape[0]
+        for i in range(10000):
+            hidden_layer = np.maximum(0, np.dot(X, W) + b) # ReLU activation function of the forward pass
+            scores = np.dot(hidden_layer, W2) + b2
+
+            # Class probs
+            exp_scores = np.exp(scores)
+            probs = exp_scores / np.sum(exp_scores, axis = 1, keepdims = True)
+
+            # We compute the loss again...
+            correct_logprobs = -np.log(probs[range(num_examples), y])
+            data_loss = np.sum(correct_logprobs) / num_examples
+            reg_loss = 0.5 * reg * np.sum(W*W) + 0.5 * reg * np.sum(W2 * W2)
+            loss = data_loss + reg_loss
+
+            if i % 1000 == 0:
+                print("Iteration %d: Loss %f" %(i, loss))
+
+            # Compute the gradient on the scores
+            dscores = probs
+            dscores[range(num_examples), y] -= 1
+            dscores /= num_examples
+
+            # And backpropagate...
+            dW2 = np.dot(hidden_layer.T, dscores)
+            db2 = np.sum(dscores, axis = 0, keepdims = True)
+            # Backprop to the hidden layer
+            dhidden = np.dot(dscores, W2.T)
+            # Backdrop the ReLU non-linearity
+            dhidden[hidden_layer <= 0] = 0
+            # And finally backprop to W and b
+            dW = np.dot(X.T, dhidden)
+            db = np.sum(dhidden, axis = 0, keepdims = True)
+
+            # Add regularization gradient contribution
+            dW2 += reg * W2
+            dW += reg * W
+
+            # Update params
+            W += -learning_rate * dW
+            b += -learning_rate * db
+            W2 += -learning_rate * dW2
+            b2 += -learning_rate * db2
+
+        # Evaluate training set accuracy
+        hidden_layer = np.maximum(0, np.dot(X, W) + b)
+        scores = np.dot(hidden_layer, W2) + b2
+        predicted_class = np.argmax(scores, axis = 1)
+
+        # Plot the result
+        h = 0.02
+        x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                            np.arange(y_min, y_max, h))
+        Z = np.dot(np.maximum(0, np.dot(np.c_[xx.ravel(), yy.ravel()], W) + b), W2) + b2
+        Z = np.argmax(Z, axis=1)
+        Z = Z.reshape(xx.shape)
+        plt.contourf(xx, yy, Z, cmap= 'viridis', alpha=0.4)
+        plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='viridis')
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.show()
+
 
 N = 100 # number of points per class
 D = 2 # dimensionality
@@ -100,7 +176,8 @@ y = np.zeros(N*K, dtype='uint8') # Label for all classes
 p = PointClassifier(N, D, K, X, y)
 p.conf_plot()
 p.plot_points()
+# Train the classifier using a linear classifier - not so good
 p.train_linear_classifier()
-
+p.train_nn()
 
     
